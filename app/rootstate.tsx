9b937069bc30addebea2bbf5ce702aefd5436514
fenri_min <fenri.mintardja@salt.co.id>
2024-05-10 09:40:16 +0700

    first commit

---
+++ app/rootstate.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {
+  MantineColorsTuple,
+  MantineProvider,
+  createTheme,
+} from "@mantine/core";
+import "@mantine/core/styles.css";
+import "./globals.css";
+
+export default function RootState({ children }: { children: React.ReactNode }) {
+  const myColor: MantineColorsTuple = [
+    "#eef3ff",
+    "#dce4f5",
+    "#b9c7e2",
+    "#94a8d0",
+    "#748dc1",
+    "#5f7cb8",
+    "#5474b4",
+    "#44639f",
+    "#39588f",
+    "#2d4b81",
+  ];
+
+  const theme = createTheme({
+    colors: {
+      myColor,
+    },
+  });
+  return <MantineProvider theme={theme}>{children}</MantineProvider>;
+}
