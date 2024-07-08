9531f7d700edc9e48bbda0bacda9e25e9cef2487
fenri_min <fenri.mintardja@salt.co.id>
2024-07-08 16:34:12 +0700

    change config vitest v2

--- vitest.config.js
+++ vitest.config.js
@@ -3,16 +3,21 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    // environment: "jsdom",
     coverage: {
       enabled: true,
       provider: "istanbul",
       exclude: ["**/style*.ts"],
-      include: ["**mockingData**"],
+      include: [
+        "**mockingData/core/domain**",
+        "**mockingData/core/data/repositories**",
+      ],
       all: false,
       statements: 100,
       lines: 100,
       branches: 100,
       functions: 100,
     },
+    passWithNoTests: true,
   },
 });
